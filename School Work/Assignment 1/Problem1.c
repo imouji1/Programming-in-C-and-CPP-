fef30bae195a0d6330f9ad595071400e2238b986
Idris Mouji <53492851+imouji1@users.noreply.github.com>
2021-09-30 04:03:45 +0200

    Create Problem1.c

---
+++ School Work/Assignment 1/Problem1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+double result; /∗ The result of our calculation ∗/
+result = (3 + 1) / 5;
+printf("The value of 4/5 is %lf\n", result);
+return 0;
+}
+
+/*  The reason the result is 0.000 is because of the way
+the compiler goes through the calculation in line 4.
+First, the compiler would add the 3 and 4 and assign that value
+to a temporary variable that safekeeps the value so far while the
+compiler is loading the accumulator. Once that is done, it divides the
+temp by the number 5, giving the result of 0. This value is zero because
+the type of the temp variable is infered from the numbers  in the
+equation, which are all integers. As an integer type, the value
+cannot have a decimal part, thus dropping it. Once this temp value
+is assigned to the result variable, the decimal point with the
+three zeros show up because the type for result is float. */
