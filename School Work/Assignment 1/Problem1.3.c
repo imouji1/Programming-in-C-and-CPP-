9dda854ff2800f97691aa16442d9ecccabbc0cb7
Idris <idris@Latest.myguest.virtualbox.org>
2023-03-01 17:57:20 +0100

    Revert "clean up"
    This reverts commit 2a0def44e67794a64a7ea27fde6c49bf8bf5af36.

---
+++ School Work/Assignment 1/Problem1.3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+	double result; /*The result of the division*/
+	int a = 5;
+	int b = 13.5;
+	result = a / b;
+	printf("The result is %f\n", result);
+	return 0;
+}
+/*The format '%f' expects argument of type 'double' but argument 2 has type 'int'*/
+
---
+++ School Work/Assignment 1/Problem1.4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 17;
+	int y = 4; 
+
+	printf("Value of x = %d\n", x);
+	printf("Value of y = %d\n", y);
+	printf("Sum of x and y  = %d\n", x+y);
+	printf("Product of x and y  = %d\n", x*y);
+	printf("Difference of x and y  = %d\n", x-y);
+	printf("Division of x and y  = %f\n", (float)x/y);
+	printf("Remainder of x and y  = %d\n", x%y);
+	
+	return 0;
+}
