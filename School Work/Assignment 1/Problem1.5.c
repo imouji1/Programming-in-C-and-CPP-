45d6c2a1f322e831f038de0f13c45f9e7caa07ed
Idris <idris@Latest.myguest.virtualbox.org>
2023-03-01 13:08:26 +0100

    intial commit

---
+++ School Work/Assignment 1/Problem1.5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 2138;
+	float y = -52.358925;
+	char z = 'G';
+	double u = 61.295339687;
+
+	printf("of x:%9d\n", x);
+	printf("of y:%11.5f\n", y);
+	printf("of z:%c\n", z);
+	printf("of u:%.7lf\n", u);
+
+	return (0);
+}
